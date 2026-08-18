55529a4c97790fc1b0389bca31ecb760d46b2fec
rishipittala1108 <rishipittala1108@gmail.com>
2026-08-18 11:25:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ formatted_agent_chain = (
 
 # --- 3. FastAPI App ---
 ##Need To Code
-app = FastAPI("Indian Weather and Cinema Agent")
+app = FastAPI(title="Indian Weather and Cinema Agent")
 add_routes(app,
            formatted_agent_chain,
            path="/agent",
